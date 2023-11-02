42e42419007d3b95292ef895dd89509bf77911f0
mjclawrence <32300703+mjclawrence@users.noreply.github.com>
2023-11-02 10:12:24 -0400

    creates assignment_05

--- assignments/assignment_05/assignment_05_cleanup.R
+++ assignments/assignment_05/assignment_05_cleanup.R
@@ -2,7 +2,7 @@ library(tidyverse)
 gss <- read_csv("/Users/lawrence/desktop/gss.csv")
 
 gss <- gss |> 
-  filter(year == 1987 | year == 2021) 
+  filter(year == 2021) 
 
 gss_subset <- gss |> 
   filter(born>0) |> 
@@ -25,12 +25,36 @@ summary(gss_subset)
 
 
 gss_test <- gss_subset |> 
-  mutate(ophrdwrk_essential = ifelse(ophrdwrk==1, 1, 0))
+  mutate(ophrdwrk_essential = ifelse(ophrdwrk==1, 1, 0),
+         opknow_essential = ifelse(opknow==1, 1, 0))
+
+gss_test <- gss_test |> 
+  mutate(born = ifelse(born == 2, "Immigrant", "Native"))
+
+oplabels <- c("Essential", "Very Important", "Fairly Important", "Not Very Important",
+              "Not Important At All")
+
+gss_test <- gss_test |> 
+  mutate(opwlth = factor(opwlth,
+                         labels = oplabels),
+         oppared = factor(oppared, labels = oplabels),
+         opeduc = factor(opeduc, labels = oplabels),
+         opclout = factor(opclout, labels = oplabels),
+         oprace = factor(oprace, labels = oplabels),
+         oprelig = factor(oprelig, labels = oplabels),
+         opsex = factor(opsex, labels = oplabels)) |> 
+  select(-c(ophrdwrk, opknow))
+
+
+write.csv(gss_test, "data/assignment_05.csv", row.names = FALSE)
+
 
 # Difference in proportion of immigrants vs non immigrants 
 # who say hard work is essential
 
 mytable <- table(gss_test$born, gss_test$ophrdwrk_essential)
+mytable
+prop.table(mytable,1)
 prop.test(mytable)
 
 # Difference in average age between immigrants and non immigrants
@@ -50,7 +74,7 @@ t.test(gss_test$yearsusa[gss_test$born==2 & gss_test$ophrdwrk_essential==1],
 # who say wealth is essential (opwlth)
 
 gss_test <- gss_test |> 
-  mutate(opwlth_essential = ifelse(opwlth==1, 1, 0))
+  mutate(opwlth_essential = ifelse(opwlth %in% c("Essential", "Very Important"), 1, 0))
 
 opwlth_table <- table(gss_test$born, gss_test$opwlth_essential)
 prop.test(opwlth_table, conf.level = .99)
@@ -59,7 +83,7 @@ prop.test(opwlth_table, conf.level = .99)
 # who say parent's education is essential (oppared)
 
 gss_test <- gss_test |> 
-  mutate(oppared_essential = ifelse(oppared==1, 1, 0))
+  mutate(oppared_essential = ifelse(oppared %in% c("Essential", "Very Important"), 1, 0))
 
 oppared_table <- table(gss_test$born, gss_test$oppared_essential)
 prop.test(oppared_table, conf.level = .99)
@@ -68,7 +92,7 @@ prop.test(oppared_table, conf.level = .99)
 # who say education is essential (opeduc)
 
 gss_test <- gss_test |> 
-  mutate(opeduc_essential = ifelse(opeduc==1, 1, 0))
+  mutate(opeduc_essential = ifelse(opeduc %in% c("Essential", "Very Important"), 1, 0))
 
 opeduc_table <- table(gss_test$born, gss_test$opeduc_essential)
 prop.test(opeduc_table, conf.level = .99)
@@ -77,7 +101,7 @@ prop.test(opeduc_table, conf.level = .99)
 # who say political connections is essential (opclout)
 
 gss_test <- gss_test |> 
-  mutate(opclout_essential = ifelse(opclout==1, 1, 0))
+  mutate(opclout_essential = ifelse(opclout %in% c("Essential", "Very Important"), 1, 0))
 
 opclout_table <- table(gss_test$born, gss_test$opclout_essential)
 prop.test(opclout_table, conf.level = .99)
@@ -86,7 +110,7 @@ prop.test(opclout_table, conf.level = .99)
 # who say being the right race is essential (oprace)
 
 gss_test <- gss_test |> 
-  mutate(oprace_essential = ifelse(oprace==1, 1, 0))
+  mutate(oprace_essential = ifelse(oprace %in% c("Essential", "Very Important"), 1, 0))
 
 oprace_table <- table(gss_test$born, gss_test$oprace_essential)
 prop.test(oprace_table, conf.level = .99)
@@ -95,7 +119,7 @@ prop.test(oprace_table, conf.level = .99)
 # who say being the right religion is essential (oprelig)
 
 gss_test <- gss_test |> 
-  mutate(oprelig_essential = ifelse(oprelig==1, 1, 0))
+  mutate(oprelig_essential = ifelse(oprelig %in% c("Essential", "Very Important"), 1, 0))
 
 oprelig_table <- table(gss_test$born, gss_test$oprelig_essential)
 prop.test(oprelig_table, conf.level = .99)
@@ -104,7 +128,7 @@ prop.test(oprelig_table, conf.level = .99)
 # who say being the right sex is essential (opsex)
 
 gss_test <- gss_test |> 
-  mutate(opsex_essential = ifelse(opsex==1, 1, 0))
+  mutate(opsex_essential = ifelse(opsex %in% c("Essential", "Very Important"), 1, 0))
 
 opsex_table <- table(gss_test$born, gss_test$opsex_essential)
 prop.test(opsex_table, conf.level = .99)
@@ -115,22 +139,25 @@ prop.test(opsex_table, conf.level = .99)
 # who say knowing right people is essential (opknow)
 
 gss_test <- gss_test |> 
-  mutate(opknow_essential = ifelse(opknow==1, 1, 0))
+  mutate(opknow_essential = ifelse(opknow<=2, 1, 0))
 
 opknow_table <- table(gss_test$born, gss_test$opknow_essential)
 prop.test(opknow_table, conf.level = .99)
 
 
+## Cleanup
+
 
 
 gss_test_imm <- gss_test |> 
-  filter(born == 2) |> 
-  mutate(imm_years = ifelse(yearsusa<10, 0, 1)) |> 
-  mutate(opknow_essential_vimp = ifelse(opknow <= 2, 1, 0))
+  filter(born == "Immigrant") |> 
+  mutate(imm_years = ifelse(yearsusa<15, 0, 1))
 
-mytable <- table(gss_test_imm$imm_years, gss_test_imm$opknow_essential_vimp)
+mytable <- table(gss_test_imm$imm_years, gss_test_imm$opknow_essential)
+mytable
 prop.table(mytable,1)
 prop.test(mytable)
 
-t.test(gss_test_imm$yearsusa[gss_test_imm$ophrdwrk_essential==0],
-       gss_test_imm$yearsusa[gss_test_imm$ophrdwrk_essential==1])
+
+
+
